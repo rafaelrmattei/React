a189ec783aae4c4057e6f2e9a9614e1d96df46fe
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-03-19 12:21:43 -0300

    Components

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,16 @@
-function App() {
+import { Post } from "./Post"
+
+export function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Post
+        author="Rafael Mattei"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias sint exercitationem numquam vel mollitia nulla rerum aliquam cumque, minus dolorum nihil eaque quis ipsam perspiciatis beatae labore earum amet laudantium."
+      />
+      <Post
+        author="Letícia Mattei"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint sed aliquid exercitationem reprehenderit saepe sequi repellat, alias praesentium temporibus, nesciunt officiis? Ea recusandae sunt praesentium voluptatibus provident dicta accusantium nihil!"
+      />
+    </>
   )
 }
-
-export default App
