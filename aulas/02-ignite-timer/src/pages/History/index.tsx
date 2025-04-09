8ac3e6e40835710468cd840abf7d4c91e1258ee6
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-04-09 13:08:23 -0300

    Finished

--- aulas/02-ignite-timer/src/pages/History/index.tsx
+++ aulas/02-ignite-timer/src/pages/History/index.tsx
@@ -6,8 +6,6 @@ import { formatDistanceToNow } from "date-fns";
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
-  
-  console.log(cycles)
 
   return (
     <HistoryContainer>
@@ -29,7 +27,7 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{formatDistanceToNow(cycle.startDate, {
+                  <td>{formatDistanceToNow(new Date(cycle.startDate), {
                     addSuffix: true,
                     locale: ptBR
                   })}</td>
