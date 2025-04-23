038855067ecf85494d4f7c01c2606656af018069
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-04-23 20:28:47 -0300

    Project started. Shadcn installed and files cleaned

--- aulas/04-pizza-shop/src/App.tsx
+++ aulas/04-pizza-shop/src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
