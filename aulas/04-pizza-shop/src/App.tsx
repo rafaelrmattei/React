fb545a3a3d537abbe329d655014833695c04b7a9
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-04-24 09:35:54 -0300

    Nav Added

--- aulas/04-pizza-shop/src/App.tsx
+++ aulas/04-pizza-shop/src/App.tsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/ThemeProvider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
       <RouterProvider router={router} />
       <Toaster richColors />
-    </>
+    </ThemeProvider>
   )
 }
