4e42e264121f9b871996da5c3e96fd5146a5f453
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-04-23 21:30:18 -0300

    React Router installed

--- aulas/04-pizza-shop/src/App.tsx
+++ aulas/04-pizza-shop/src/App.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { RouterProvider } from 'react-router'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
---
+++ aulas/04-pizza-shop/src/routes.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from 'react-router'
+
+import { AppLayout } from './pages/_layouts/App'
+import { AuthLayout } from './pages/_layouts/Auth'
+import { Dashboard } from './pages/app/Dashboard'
+import { SignIn } from './pages/auth/SignIn'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+    ],
+  },
+])
