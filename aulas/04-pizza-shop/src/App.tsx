2cd354716a85c09db68b82940e06e87ba7df19ad
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-04-24 17:27:10 -0300

    Added envs, react-query, axios

--- aulas/04-pizza-shop/src/App.tsx
+++ aulas/04-pizza-shop/src/App.tsx
@@ -1,13 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/ThemeProvider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
       <Toaster richColors />
     </ThemeProvider>
   )
