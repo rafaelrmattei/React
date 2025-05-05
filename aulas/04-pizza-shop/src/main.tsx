dca895da231b82245742f6d454d9464d8052e964
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-05-05 14:19:42 -0300

    Starting with mocks

--- aulas/04-pizza-shop/src/main.tsx
+++ aulas/04-pizza-shop/src/main.tsx
@@ -3,10 +3,13 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { enableMSW } from './api/mocks/index.ts'
 import { App } from './app.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
