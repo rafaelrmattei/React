a8486065151d2904130037adbc7679de13cb4ee3
Rafael Mattei <74362290+rafaelrmattei@users.noreply.github.com>
2025-05-05 11:49:09 -0300

    Test environment configured

---
+++ aulas/04-pizza-shop/src/components/OrderStatus.spec.tsx
@@ -0,0 +1,5 @@
+describe('Test environment', () => {
+  it('should run a basic test', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
---
+++ aulas/04-pizza-shop/vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest/config" />
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './test/setup.ts',
+  },
+})
