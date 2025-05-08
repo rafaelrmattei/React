15c5a1d9d031a5426299c3c9d152c4d48a485766
Rafael Mattei <rafaelrmattei@gmail.com>
2025-05-08 15:28:00 -0300

    Layout ready

--- aulas/05-ignite-shop/src/pages/index.tsx
+++ aulas/05-ignite-shop/src/pages/index.tsx
@@ -1,25 +1,66 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "@/styles/pages/home"
+import { useKeenSlider } from 'keen-slider/react'
+import Image from "next/image"
+import 'keen-slider/keen-slider.min.css'
+import { stripe } from "@/lib/stripe"
+import { GetStaticProps } from "next"
+import Stripe from "stripe"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
 
-  span: {
-    fontWeight: 'bold'
-  },
+export default function Home({ products }: HomeProps) {
+  const [ sliderRef ] = useKeenSlider({
+    slides: {
+      perView: 3,      
+      spacing: 48,
+    }
+  })
 
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  },
-})
-
-export default function Home() {
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={520} alt={product.name} />
+
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: ((price.unit_amount ?? 0) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }),
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2
+  }
 }
