450e64d4abd31f8bd6a54f16638a918652813f1f
Rafael Mattei <rafaelrmattei@gmail.com>
2025-05-16 15:06:18 -0300

    Tokens

--- aulas/06-design-system/packages/docs/.storybook/preview.js
+++ aulas/06-design-system/packages/docs/.storybook/preview.js
@@ -1,3 +1,5 @@
+import { themes } from '@storybook/theming'
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -6,6 +8,9 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      theme: themes.dark,
     },
   },
 }
