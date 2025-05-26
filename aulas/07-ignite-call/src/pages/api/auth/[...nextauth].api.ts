9a35cb98826e3b5c3499a776b270cac51f98bdb3
Rafael Mattei <rafaelrmattei@gmail.com>
2025-05-26 16:28:42 -0300

    Added personalized adapter for next auth

--- aulas/07-ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ aulas/07-ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -1,30 +1,53 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          //Pego no google - urls de escopos exigidos pela nossa aplicação que são necessários autorização
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+import { PrismaAdapter } from '../../../lib/auth/prisma-adapter'
+
+export function buildNextAuthOptions(req: NextApiRequest, res: NextApiResponse): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res), //Adapter criado do zero para fins de estudo e porque é diferente do padrão do next auth
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            //Pego no google - urls de escopos exigidos pela nossa aplicação que são necessários autorização
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+          },
         },
-      },
-    }),
-  ],
-  callbacks: {
-    // Verifica o retorno do google pra ver se o usuário autorizou o uso do calendário
-    async signIn({ account }) {
-      if (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
-        return '/register/connect-calendar/?error=permissions'
-      }
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+    callbacks: {
+      // Verifica o retorno do google pra ver se o usuário autorizou o uso do calendário
+      async signIn({ account }) {
+        if (!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
+          return '/register/connect-calendar/?error=permissions'
+        }
 
-      return true //Se der erro manda pra connect-calendar com error=permissions, senão retorna que está tudo certo
+        return true //Se der erro manda pra connect-calendar com error=permissions, senão retorna que está tudo certo
+      },
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
     },
-  },
+  }
 }
 
-export default NextAuth(authOptions)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
